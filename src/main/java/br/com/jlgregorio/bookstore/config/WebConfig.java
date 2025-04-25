f2ea32bbcf5c6982508b1007b5ef1e50bf5448e8
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2025-04-25 09:04:24 -0300

    Cors implementation is OK

---
+++ src/main/java/br/com/jlgregorio/bookstore/config/WebConfig.java
@@ -0,0 +1,19 @@
+package br.com.jlgregorio.bookstore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+       registry.addMapping("/**")
+               .allowedMethods("POST", "PUT", "GET", "DELETE",
+                       "OPTIONS", "HEAD", "TRACE", "CONNECT")
+               .allowedOrigins("http://localhost/bookstore:8888", "http://www.fatecjales.edu.br");
+    }
+}
